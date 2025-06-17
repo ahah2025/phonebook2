f9b81081d129cb3ebdae490e10f51c05decf5afe
ahah2025 <adghty61@naver.com>
2025-06-17 12:39:57 +0900

    2025-06-17

--- src/main/java/com/javaex/controller/Phonebookcontroller.java
+++ src/main/java/com/javaex/controller/Phonebookcontroller.java
@@ -3,6 +3,7 @@ package com.javaex.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,24 +19,61 @@ public class Phonebookcontroller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	// 생성자
-	public Phonebookcontroller() {
-	}
+	public Phonebookcontroller() {	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// 로직
+		//작동했는지 확인용
 		System.out.println("PhoneBookController"); // 콘솔 출력되는 프린트
 
-		// db 데이터 가져온다 --> list
-		PhonebookDAO phonebookDAO = new PhonebookDAO();
-		List<PersonVO> personList = phonebookDAO.personSelect();
-
-		System.out.println(personList);
-		// html + list --> 두개를 섞어야 함
 		
+		//action 파라미터의 값이 뭔지 알아야됨
+		String action = request.getParameter("action");
+		System.out.println(action);//업무구분
 		
-		// 응답문서 바디 추가한다
+		if("list".equals(action)) { //리스트업무
+			System.out.println("리스트");
+			//리스트//////////////////////////////////////////////////////////////
+			
+			// db 데이터 가져온다 --> list
+			PhonebookDAO phonebookDAO = new PhonebookDAO();
+			List<PersonVO> personList = phonebookDAO.personSelect();
 
+			System.out.println(personList);
+			
+			//저밑에 있는 list.jsp에게 후반일을 시킨다
+			// 1) request객체에 데이터를 넣어준다
+			request.setAttribute("pList",personList);
+			//                     별명  , 0x333  을 넣어준다
+			
+			
+			
+			// 2) list.jsp에 request 객체와 reponse 객체를 보낸다
+			// ** forward 중요한 기술: 전달하는 기술 
+			RequestDispatcher rd = request.getRequestDispatcher("/list.jsp");
+			rd.forward(request, response);
+			
+			// ** forward 중요한 기술: 전달하는 기술 
+			
+			////////////////////////////////////////////////////////////////
+			
+			
+		}else if("wform".equals(action)) { //등록폼업무 (등록업무랑 구별할 것)
+			System.out.println("등록폼");
+	
+			//등록폼을 응답해야한다
+			//1)DB관련 할일이 없다 - 안하면된다
+			
+			//2)jsp에게 화면을 그리게 한다(포워드)
+			//writeForm.jsp 포워드한다
+			RequestDispatcher rd = request.getRequestDispatcher("/writeForm.jsp");
+			rd.forward(request, response);
+		
+		}else if("write".equals(action)) { //등록업무        equals 중요!!!!
+			System.out.println("등록");
+			
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ src/main/webapp/list.jsp
@@ -0,0 +1,62 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<%@ page import="java.util.List"    %>    
+<%@ page import="com.javaex.vo.PersonVO"  %>
+
+<% //자바문법
+	List<PersonVO> personList = (List<PersonVO>)request.getAttribute("pList");
+	//                          형변환 해줘야 함
+	//parameter영역에 임의로 데이터를 추가하여 사용할 수 없다.(요청시 parameter값은 결정됨)
+	//prrameter영역의 값과 attribute영역의 값을 구분하여 사용할 수 있어야 한다.
+	
+    System.out.println("여기는 jsp");
+    System.out.println(personList);
+%>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Insert title here</title>
+	</head>
+	<body>
+		<h1>주소록</h1>
+	
+		<h2>전화번호 리스트</h2>
+		<p>입력한 정보 내역입니다.</p>
+		
+		<%
+			for(int i=0; i<personList.size(); i++){
+		%>	
+				
+			
+		<table border ="1">
+			<tbody>
+				<tr>	
+					<td>이름(name)</td>
+					<td><%=personList.get(i).getName() %></td>
+				</tr>
+				<tr>
+	            	<td>핸드폰(hp)</td>
+	                <td><%=personList.get(i).getHp() %></td>
+            	<tr>
+            	<tr>
+               		<td>회사(company)</td>
+               		<td><%=personList.get(i).getCompany() %></td>
+            	<tr>      
+			</tbody>
+		</table>
+		
+		<br>
+		<%	
+		}	
+		%>
+		
+		[주소록 작성폼 이동]:주소창에 aaa.html 입력하시고 엔터입력
+		<br>
+		
+		<a href="/aaa.html">주소록 작성폼 이동</a>
+	
+	</body>
+</html>
